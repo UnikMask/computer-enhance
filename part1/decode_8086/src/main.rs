4bd9eac7c2d87770c3a021a73c94ce7dce5cd541
UnikMask <alexandre.guerville@hotmail.com>
2025-03-09 12:01:06 +0000

    feat(part1): Implement homework from part 1

---
+++ part1/decode_8086/src/main.rs
@@ -0,0 +1,135 @@
+use std::{env, fs::File, io::Read};
+
+const BASE_INSTR_SIZE: usize = 2;
+
+fn main() {
+    let filename = env::args().nth(1).expect("No filename provided!");
+
+    let mut f = match File::open(&filename) {
+        Ok(file) => file,
+        Err(e) => {
+            panic!("Failed to open {} - {e}", &filename)
+        }
+    };
+
+    let mut done = false;
+    println!("bits 16");
+    while !done {
+        let mut bytes: [u8; BASE_INSTR_SIZE] = [0; BASE_INSTR_SIZE];
+
+        // Read 1st and 2nd byte
+        match f.read(&mut bytes[0..2]) {
+            Ok(2) => {}
+            Err(e) => panic!("IO Error occured - {e}"),
+            _ => {
+                done = true;
+                continue;
+            }
+        }
+
+        // Split 1st byte
+        let (instr, d, w) = (
+            bytes[0] >> 2,
+            ((bytes[0] >> 1) & 1) == 1,
+            (bytes[0] & 1) == 1,
+        );
+
+        // Split 2nd byte
+        let (r#mod, reg, rm) = (bytes[1] >> 6, (bytes[1] >> 3) & 0b111, bytes[1] & 0b111);
+
+        let (src, dst) = if d {
+            (
+                get_reg_code(reg, w).to_owned(),
+                get_rm_code(rm, w, r#mod, &mut f),
+            )
+        } else {
+            (
+                get_rm_code(rm, w, r#mod, &mut f),
+                get_reg_code(reg, w).to_owned(),
+            )
+        };
+
+        match instr {
+            0b100010 => println!("mov {dst}, {src}"),
+            _ => panic!("Not supported yet!"),
+        }
+    }
+}
+
+fn get_rm_code(rm: u8, wide: bool, r#mod: u8, f: &mut File) -> String {
+    match r#mod {
+        0b11 => get_reg_code(rm, wide).to_owned(),
+        0b01 => {
+            let mut offset: [u8; 1] = [0; 1];
+            assert!(
+                matches!(f.read(&mut offset), Ok(1)),
+                "Could not read next byte!"
+            );
+            get_rm_code_with_offset(rm, offset[0].into())
+        }
+        0b10 => {
+            let mut offset: [u8; 2] = [0; 2];
+            assert!(
+                matches!(f.read(&mut offset), Ok(2)),
+                "Could not read next byte!"
+            );
+            get_rm_code_with_offset(rm, u16::from(offset[0]) + (u16::from(offset[1]) << 8))
+        }
+        0b00 => match rm {
+            0 => "(BX)+(SI)".to_string(),
+            1 => "(BX)+(DI)".to_string(),
+            2 => "(BP)+(SI)".to_string(),
+            3 => "(BP)+(DI)".to_string(),
+            4 => "(SI)".to_string(),
+            5 => "(DI)".to_string(),
+            6 => {
+                let mut address: [u8; 2] = [0; 2];
+                assert!(
+                    matches!(f.read(&mut address), Ok(2)),
+                    "Could not read next byte!"
+                );
+                format!("{:x}", u16::from(address[0]) + (u16::from(address[1]) << 8))
+            }
+            7 => "(BX)+(SI)".to_string(),
+            _ => panic!("Impossible!"),
+        },
+        _ => panic!("Impossible!"),
+    }
+}
+
+fn get_rm_code_with_offset(rm: u8, offset: u16) -> String {
+    match rm {
+        0 => format!("(BX)+(SI)+{offset:x}"),
+        1 => format!("(BX)+(DI)+{offset:x}"),
+        2 => format!("(BP)+(SI)+{offset:x}"),
+        3 => format!("(BP)+(DI)+{offset:x}"),
+        4 => format!("(SI)+{offset:x}"),
+        5 => format!("(DI)+{offset:x}"),
+        6 => format!("(BP)+{offset:x}"),
+        7 => format!("(BX)+{offset:x}"),
+        _ => panic!("Impossible!"),
+    }
+}
+
+/// Assumes MOD == 11
+fn get_reg_code(instr: u8, wide: bool) -> &'static str {
+    match (wide, instr) {
+        (true, 0) => "AL",
+        (true, 1) => "AL",
+        (true, 2) => "AL",
+        (true, 3) => "AL",
+        (true, 4) => "AL",
+        (true, 5) => "AL",
+        (true, 6) => "AL",
+        (true, 7) => "AL",
+        (false, 0) => "AL",
+        (false, 1) => "AL",
+        (false, 2) => "AL",
+        (false, 3) => "AL",
+        (false, 4) => "AL",
+        (false, 5) => "AL",
+        (false, 6) => "AL",
+        (false, 7) => "AL",
+        _ => panic!("Impossible!"),
+    }
+}
